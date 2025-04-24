f2ee37bf58d71e7de2768619fb4ef3f121ced7d1
faleialina <august3@mail.ru>
2025-04-24 21:21:28 +0300

    string 19-30

--- 3 string/main.go
+++ 3 string/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 // "strings"
 
 // "bufio"
@@ -161,18 +156,152 @@ func main() {
 	// }
 
 	// 19. В предложении I love apples, заменить apples на oranges.
+
+	// str := "I love apples"
+	// fmt.Println(strings.Replace(str, "apples", "oranges", -1))
+
 	// 20. Учитывать только латинские гласные a, e, i, o, u.
+
 	// 21. Инверсия регистра (если буква — заглавная, сделать строчной и наоборот) GoLang → gOlANG
+
+	// str := "GoLang"
+	// result := []rune(str)
+	// for i, char := range result {
+	// 	if unicode.IsUpper(char) {
+	// 		result[i] = unicode.ToLower(char)
+	// 	} else if unicode.IsLower(char) {
+	// 		result[i] = unicode.ToUpper(char)
+	// 	}
+	// }
+	// fmt.Printf("Исходная строка: %s\n", str)
+	// fmt.Printf("Результат: %s\n", string(result))
+
 	// 22. Проверка, является ли строка палиндромом. казак, шалаш → true
+
+	// words := []string{"казак", "шалаш"}
+
+	// for _, word := range words {
+
+	// 	clean := strings.ToLower(strings.Replace(word, " ", "", -1))
+
+	// 	isPalindrome := true
+	// 	for i := 0; i < len(clean)/2; i++ {
+	// 		if clean[i] != clean[len(clean)-1] {
+	// 			isPalindrome = false
+	// 			break
+	// 		}
+	// 	}
+
+	// 	fmt.Printf("'%s' → %t\n", word, isPalindrome)
+	// }
+
 	// 23. Поиск самого длинного слова в строке go is an expressive and concise language → expressive
+
+	// str := "go is an expressive and concise language"
+
+	// words := strings.Fields(str)
+
+	// longest := ""
+	// for _, word := range words {
+	// 	if len(word) > len(longest) {
+	// 		longest = word
+	// 	}
+	// }
+
+	// fmt.Printf("Самое длинное слово: \"%s\" (длина: %d)\n", longest, len(longest))
+
 	// 24. Найти все уникальные символы в строке aabccdee → a b c d e
+
+	// str := "aabccdee"
+	// unique := make(map[rune]bool)
+
+	// for _, char := range str {
+	// 	unique[char] = true
+	// }
+
+	// // Выводим результат
+	// fmt.Print("Уникальные символы: ")
+	// for char := range unique {
+	// 	fmt.Printf("%c ", char)
+	// }
+
 	// 25. Подсчитать количество цифр в строке Пример: в abc123def456 — 6 цифр.
+
+	// str := "abc123def456"
+	// count := 0
+	// for i := 0; i < len(str); i++ {
+	// 	c := str[i]
+	// 	if c >= '0' && c <= '9' {
+	// 		count++
+	// 	}
+	// }
+	// fmt.Printf("В строке \"%s\" содержится %d цифр\n", str, count)
 	// 26. Сделать первую букву каждого слова заглавной Пример: go is fun → Go Is Fun.
+
+	// str := "go is fun"
+
+	// words := strings.Fields(str)
+
+	// for i, word := range words {
+	// 	if len(word) > 0 {
+
+	// 		first := []rune(word)[0]
+	// 		if unicode.IsLower(first) {
+	// 			words[i] = string(unicode.ToUpper(first)) + word[1:]
+	// 		}
+	// 	}
+	// }
+
+	// result := strings.Join(words, " ")
+	// fmt.Println(result)
+
 	// 27. Подсчитать количество символов пунктуации Для строки Hello, world! How are you? должно
 	// быть 3 (,, !, ?).
+
+	// str := "Hello, world! How are you?"
+	// count := 0
+
+	// for _, char := range str {
+	// 	if unicode.IsPunct(char) {
+	// 		count++
+	// 	}
+	// }
+
+	// fmt.Printf("Количество символов пунктуации: %d\n", count)
+
 	// 28. Найти подстроки, начинающиеся с заглавной буквы Пример: Welcome To Go Language →
 	// Welcome, To, Go, Language
+
+	// str := "Welcome To Go Language"
+	// var result []string
+
+	// words := strings.Fields(str)
+
+	// for _, word := range words {
+	// 	if len(word) > 0 && unicode.IsUpper([]rune(word)[0]) {
+	// 		result = append(result, word)
+	// 	}
+	// }
+
+	// fmt.Println(strings.Join(result, ", "))
+
 	// 29. Проверить, является ли строка числом Примеры: 123 → true, 12a3 → false
 	// Метод: strconv.Atoi
+
+	// inputs := []string{"123", "12a3"}
+
+	// for _, s := range inputs {
+	// 	_, err := strconv.Atoi(s)
+	// 	fmt.Printf("%q → %v\n", s, err == nil)
+	// }
+
 	// 30. Удалить из строки все согласные Пример: banana → aaa
+
+	// s := "bananaE"
+	// for _, c := range s {
+	// 	if c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+	// 		c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' {
+	// 		fmt.Print(string(c))
+	// 	}
+	// }
 }
